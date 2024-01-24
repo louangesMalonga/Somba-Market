b79f0c53f32e8e871e5120f59be7e0b96185d70f
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-01-24 18:09:51 +0100

    Commit 1 - 24/01/24
    Organisation projet

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include_once("shortcut/header.php");
+?>
+
+
+<?php
+include_once("shortcut/footer.php");
+?>
---
+++ shortcut/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=width-device, initial-scale=1">
+    <title>Soumba</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+
+<body>
