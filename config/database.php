a7bc9a4476841b9cdc02d55470296dfc4e9fef08
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-02-22 18:11:25 +0100

    Restart project

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+$HOST = "localhost";
+$DB = "soumba";
+$USER = "root";
+$PASS = "";
+try {
+    $connect = new PDO(
+        "mysql:host=" . $HOST . ";dbname=" . $DB . ";charset=utf-8",
+        $USER,
+        $PASS
+    );
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    echo "Connect";
+} catch (Exception $e) {
+    echo $e->getMessage() . "in line" . $e->getLine();
+}
