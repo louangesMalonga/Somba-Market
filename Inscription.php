73bbd29a4142ae9e82714f2a5468092402d61526
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-02-29 15:07:19 +0100

    Page de Connexion et d'enregistrement

---
+++ Inscription.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'inscription</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body class="bg-light">
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-lg-4 bg-white m-auto rounded-top">
+                    <h2 class="text-center"> Inscription </h2>
+                    <p class="text-center text-muted load"> Simple et Rapide</p>
+                    <form action="">
+                       
+                         <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa fa-user"></i> </span>
+                            <input type="text" class="form-control" placeholder="Nom">
+                        </div>
+                         <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa fa-user"></i> </span>
+                            <input type="text" class="form-control" placeholder="Prenom">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa fa-phone"></i> </span>
+                            <input type="text" class="form-control" placeholder="Telphone">
+                        </div>
+                         <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa fa-envelope"></i> </span>
+                            <input type="text" class="form-control" placeholder="E-mail">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa fa-lock"></i> </span>
+                            <input type="text" class="form-control" placeholder="Mot de passe">
+                        </div>
+                         <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa fa-lock"></i> </span>
+                            <input type="text" class="form-control" placeholder="Confirmer Mot de passe">
+                        </div>
+                        <div class="d-grid">
+                            <button type="button" class="btn btn-success"> s'inscrire</button>
+                            <p class="text-center text-muted "> 
+                                En cliquant sur s'inscrire, vous acceptez <a href=""> nos conditions générales </a>, notre <a href="">politique  de confidentialité </a> et notre <a href=""> politique d'utilisation </a>des cookies  
+                            </p>
+                            <p class="text-center"> Avez vous déjà un compte? <a href="connexion.html"> connexion </a> </p>
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
+<script src="bootstrap.bundle.js"></script>
---
+++ connexion.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Connexion</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body class="bg-light">
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-lg-4 bg-white m-auto rounded-top">
+                    <h2 class="text-center"> Connexion </h2>
+                    <p class="text-center text-muted load"> Connectez vous !</p>
+                    <form action="">
+                         <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa fa-envelope"></i> </span>
+                            <input type="text" class="form-control" placeholder="E-mail">
+                        </div>
+                         <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa fa-lock"></i> </span>
+                            <input type="text" class="form-control" placeholder="Mot de passe">
+                        </div>
+                        <div class="d-grid">
+                            <button type="button" class="btn btn-success"> se connecter</button>
+                            <p class="text-center"> Vous n'avez pas de compte? <a href="Inscription.html"> S'inscrire </a> </p>
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
+<script src="bootstrap.bundle.js"></script>
